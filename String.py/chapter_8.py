66e8abe5f5047a97a2e195f898d69b6fcd7549fe
Muhammad_Haris <hariscool987@gmail.com>
2025-12-12 02:29:59 -0800

    Delete String.py directory

--- String.py/chapter_8.py
+++
@@ -1,9 +0,0 @@
-ingrendints = ["water","milk","black tea"]
-ingrendints.append("sugar")
-
-ingrendints.remove("water")
-
-print(ingrendints)
-
-spice_options = ["ginger","cardomam"]
-chia_ingredints = ["water","milk"]
